21b7a9db8673a7d1f8805072eace90b858008908
Alexandra Akhundova <alexsandra.akhundova@yandex.ru>
2025-08-28 21:40:02 +0300

    itcommunity-page added React

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import CCEmitPage from '../pages/CCEmitPage/CCEmitPage';
 import '../styles/App.css';
 import CCKEmitPage from '../pages/CCKEmitPage/CCKEmitPage';
 import ClubsPage from '../pages/ClubsPage/ClubsPage';
+import ITCommunityPage from '../pages/ITCommunityPage/ITCommunityPage';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/pages/ccemit" element={<CCEmitPage />}/>
         <Route path="/pages/cckemit" element={<CCKEmitPage />}/>
         <Route path="/pages/clubs" element={<ClubsPage/>}/>
+        <Route path='/pages/clubs/itcommunity' element={<ITCommunityPage/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ITCommunityPage/ITCommunityPage.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./ITCommunityPage.css";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import usePageTitle from "../../hooks/usePageTitle";
+import podcastBackground from "../../assets/images/PodcastBackground.png";
+import podcastFoto1 from "../../assets/images/PodcastFoto-1.png";
+import podcastFoto2 from "../../assets/images/PodcastFoto-2.png";
+import BITFoto from "../../assets/images/BITFoto.png";
+import BITCircles from "../../assets/images/BITCircles.svg";
+import pulsarFoto from "../../assets/images/PulsarFoto.png";
+
+const ITCommunityPage = () => {
+  usePageTitle("Цифровое комьюнити");
+
+  return (
+    <>
+      <Header title="Цифровое комьюнити"/>
+      <main className="itcommunity-page__main">
+        <section className="itcommunity-page__section-reverse">
+            <p className="itcommunity-page__bit-text">Мы объединяем все диджитал-проекты под одной крышей и создаём среду, в которой каждый студент Президентской Академии, который интересуется IT и креативными индустриями, сможет найти единомышленников или даже реализовать собственную инициативу.</p>
+            <section className="itcommunity-page__section__podcast">
+                <img
+                    src={podcastBackground}
+                    className="itcommunity-page__podcast__background"
+                />
+                <section className="itcommunity-page__section__podcast__part-1">
+                    <img
+                        src={podcastFoto1}
+                        className="itcommunity-page__podcast__fotos"
+                    />
+                    <section className="itcommunity-page__podcast__name">
+                        <h2 className="itcommunity-page__name__podcast">Подкаст</h2>
+                        <h2 className="itcommunity-page__name__name-podcast">«Пожужжим?»</h2>
+                    </section>
+                </section>
+                <section className="itcommunity-page__section__podcast__part-2">
+                    <img
+                        src={podcastFoto2}
+                        className="itcommunity-page__podcast__fotos"
+                    />
+                </section>
+            </section>
+
+        </section>
+        
+        <section className="itcommunity-page__section__products">
+            <section className="itcommunity-page__product-item">
+                <img
+                    className="itcommunity-page__product-foto"
+                    src={BITFoto}
+                />
+                <section className="itcommunity-page__product__description">
+                    <h2 className="itcommunity-page__product__name">Цифровой форум BIT</h2>
+                    <p className="itcommunity-page__product__text">«BIT» — это время, когда Академия становится уникальной площадкой для спикеров из индустрий. Ты узнаешь всё о старте карьеры и продвижении в сфере цифровых технологий, о ведении проектов и об основах бизнеса.</p>
+                </section>
+                <img
+                    className="itcommunity-page__background__circles"
+                    src={BITCircles}
+                />
+            </section>
+            <section className="itcommunity-page__product-item product-reverse">
+                <img
+                    className="itcommunity-page__product-foto"
+                    src={pulsarFoto}
+                />
+                <section className="itcommunity-page__product__description">
+                    <h2 className="itcommunity-page__product__name">Телеграмм-бот Пульсар</h2>
+                    <p className="itcommunity-page__product__text">В этом проекте мы создали специального бота, который поможет вам погрузиться в завораживающую вселенную мероприятия.</p>
+                </section>
+            </section>
+        </section>
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default ITCommunityPage;
